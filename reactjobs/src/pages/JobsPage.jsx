b73504aae03aa613aaee08752525ef31c2444443
Wilsonli33092 <90936549+WilsonLi33092@users.noreply.github.com>
2025-01-15 00:06:16 -0500

    finished job site

---
+++ reactjobs/src/pages/JobsPage.jsx
@@ -0,0 +1,12 @@
+import JobListings from "../components/JobListings"
+
+const JobsPage = () => {
+    return(
+        <>
+        <section className="bg-blue-50 px-4 px-6">
+            <JobListings/>
+        </section>
+        </>
+    )
+}
+export default JobsPage
